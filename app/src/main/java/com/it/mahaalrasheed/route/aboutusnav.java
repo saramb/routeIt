7d07c2b6e149d0638599af6d11f4eb2bd3da6333
shaljarbou <shaljarbou@gmail.com>
2016-03-13 11:48:22 +0300

    najat

--- app/src/main/java/com/it/mahaalrasheed/route/aboutusnav.java
+++ app/src/main/java/com/it/mahaalrasheed/route/aboutusnav.java
@@ -8,7 +8,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class aboutusnav extends AppCompatActivity
@@ -39,13 +38,6 @@ public class aboutusnav extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.aboutusnav, menu);
-        return true;
     }
 
     @Override
--- app/src/main/java/com/it/mahaalrasheed/route/loginnav.java
+++ app/src/main/java/com/it/mahaalrasheed/route/loginnav.java
@@ -8,7 +8,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -159,12 +158,7 @@ public class loginnav extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.loginnav, menu);
-        return true;
-    }
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
