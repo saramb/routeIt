1364c04567f50c7dc12ba530412ae09f1d2d89aa
saramb <saramb05@gmail.com>
2016-03-14 11:13:29 +0300

    sara TEST

--- app/src/main/java/com/it/mahaalrasheed/route/aboutusnav.java
+++ app/src/main/java/com/it/mahaalrasheed/route/aboutusnav.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-//SARA TEST
 
 public class aboutusnav extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
